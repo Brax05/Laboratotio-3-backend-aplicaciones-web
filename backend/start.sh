88b995c83fabd4342ed56cce47a73ea6ea4a0817
Brandon muñoz <101955710+Brax05@users.noreply.github.com>
2025-11-02 15:50:23 -0300

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
- 
-# Este script es para forzar la detección del lenguaje Python
-# y ejecutar el comando de inicio definido en el Procfile.
- 
-# Instalar dependencias
-pip install -r requirements.txt
- 
-# Ejecutar el comando web del Procfile
-exec gunicorn app:app
+unicorn app:app
